29b351a6f2201b73b8d55c8fbc42feeeeaeefade
GoldenAnpu <sych.sergius@gmail.com>
2023-02-11 03:22:48 +0100

    add path for detailed_weather view

--- weather/urls.py
+++ weather/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.main_weather, name='main_weather'),
     path('current_weather', views.current_weather, name='current_weather'),
+    path('current_weather/<str:city>', views.detailed_weather, name='detailed_weather'),
     path('forecast', views.forecast, name='forecast'),
 ]
