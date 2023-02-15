81604cc9c6353a20246f8127e54cd323369cf1fc
GoldenAnpu <sych.sergius@gmail.com>
2023-02-15 02:00:05 +0100

    add empty and regexp validation

--- weather/forms.py
+++ weather/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .weather_services import Weather
 import asyncio
 from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator
 
 
 def city_not_found(city):
@@ -10,18 +11,28 @@ def city_not_found(city):
         raise ValidationError(f"City not found. Please enter a valid city name and try again.", code='city_not_found')
 
 
+def empty_field(city):
+    if city == 'empty':
+        raise ValidationError('You are trying to submit empty field')
+
+
 class CityInputForm(forms.Form):
-    city_input_field = forms.CharField(
-                                        required=False,
-                                        label='',
-                                        widget=forms.TextInput(
-                                            attrs={'type': "text",
-                                                   'class': 'form-control',
-                                                   'placeholder': 'London',
-                                                   'aria-label': 'City',
-                                                   'aria-describedby': 'addon_city',
-                                                   'id': 'city_input_field',
-                                                   'name': 'city_input_field'}),
-                                        max_length=30,
-                                        validators=[city_not_found]
-                                        )
+    city_input_field = forms.CharField(empty_value='empty',
+                                       required=False,
+                                       label='',
+                                       widget=forms.TextInput(
+                                           attrs={'type': "text",
+                                                  'class': 'form-control',
+                                                  'placeholder': 'London',
+                                                  'aria-label': 'City',
+                                                  'aria-describedby': 'addon_city',
+                                                  'id': 'city_input_field',
+                                                  'name': 'city_input_field'}),
+                                       max_length=33,
+                                       validators=[RegexValidator(
+                                           regex=r'^[A-Za-z]+$',
+                                           message='Field must contain only letters and numbers.'
+                                       ),
+                                           empty_field,
+                                           city_not_found, ]
+                                       )
